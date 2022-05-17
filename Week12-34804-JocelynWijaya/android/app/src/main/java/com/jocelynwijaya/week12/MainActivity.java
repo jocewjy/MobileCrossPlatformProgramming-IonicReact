3640bd90ca83729a84155ce7136587ccd9a3e8b0
Jocelyn Wijaya <jocelyn.wijaya@student.umn.ac.id>
2022-05-17 15:14:08 +0700

    Week12

---
+++ Week12-34804-JocelynWijaya/android/app/src/main/java/com/jocelynwijaya/week12/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jocelynwijaya.week12;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Week12-34804-JocelynWijaya/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jocelynwijaya.week12',
+  appName: 'week12',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
